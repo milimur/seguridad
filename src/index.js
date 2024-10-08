ffb7c03a445497f67efd63a3d809ccf49cc9c620
milimurua <milagrosmurua2004@gmail.com>
2024-10-08 20:57:48 -0300

    added new funtion to index

--- src/index.js
+++ src/index.js
@@ -36,6 +36,54 @@ app.get("/search", async (req, res, next) => {
     }
 });
 
+app.get("/channels", async (req, res, next) => {
+    try {
+        const searchQuery = req.query.channel?.trim().toLowerCase();
+
+        // Validate the search query
+        if (!searchQuery) {
+            return res.status(400).json({ error: 'El parámetro "search_query" es requerido y no puede estar vacío.' });
+        }
+        
+        // Perform the search using the YouTube API from googleapis
+        const response = await youtube.search.list({
+            part: "snippet",
+            q: searchQuery,
+            type: "channel",  // Set type to "channel"
+            maxResults: 4,
+        });
+        
+        res.json(response.data);
+    } catch (err) {
+        next(err); 
+    }
+});
+
+//http://localhost:3000/comments?videoId=HL3oCZXFoVs
+app.get("/comments", async (req, res, next) => {
+    try {
+        const videoId = req.query.videoId?.trim();
+
+        // Validate the videoId
+        if (!videoId) {
+            return res.status(400).json({ error: 'El parámetro "videoId" es requerido y no puede estar vacío.' });
+        }
+
+        // Perform the API call to retrieve comment threads
+        const response = await youtube.commentThreads.list({
+            part: "snippet,replies", 
+            videoId: videoId,  
+            maxResults: 5,  
+        });
+
+        res.json(response.data);
+    } catch (err) {
+        next(err);
+    }
+});
+
 app.listen(port, () => {
     console.log(`App escuchando en http://localhost:${port}`);
 });
+
+module.exports = app;
